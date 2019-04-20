bbc3630c9528d4d37254c1e9262c1737f41e00fb
Patrick Stover <pstover@twitter.com>
2019-04-20 16:36:51 -0600

    Extract into python project

---
+++ gentrification/helpers.py
@@ -0,0 +1,80 @@
+import random
+
+import numpy as np
+
+import commercial as c
+import person as p
+import residential as r
+import street as s
+
+
+def decision(probability):
+  return random.random() < probability
+
+
+def pop(n):
+  l = 0.2
+  p = 1 - np.exp(-l * n)
+  if p > 0.1:
+    return p
+  else:
+    return 0.1
+
+
+def e_stat(p):
+  if p <= 0.3:
+    return 0
+  if 0.3 < p <= 0.6:
+    return 1
+  if p > 0.6:
+    return 2
+
+
+def people_on_street(model):
+  street_visits = [agent.number_of_visitors for agent in model.schedule.agents if isinstance(agent, s.Street)]
+  return sum(street_visits)
+
+
+def people_in_neighbourhood(model):
+  people = [agent for agent in model.schedule.agents if isinstance(agent, p.Person) and agent.in_neighbourhood]
+  return len(people)
+
+
+def med_property_value(model):
+  prop_vals = [prop.property_value for prop in model.schedule.agents if isinstance(prop, r.Residential)]
+  return np.median(prop_vals)
+
+
+def med_econ_status_res(model):
+  econ_stats = [agent.econ_status for agent in model.schedule.agents if
+                isinstance(agent, p.Person) and agent.resident_status]
+  return np.median(econ_stats)
+
+
+def med_econ_status_visitors(model):
+  econ_stats = [agent.econ_status for agent in model.schedule.agents if
+                isinstance(agent, p.Person) and not agent.resident_status and agent.in_neighbourhood]
+  return np.median(econ_stats)
+
+
+def yelpers(model):
+  yelpers = [agent for agent in model.schedule.agents if isinstance(agent, p.Person) and agent.yelped]
+  for agent in yelpers:
+    agent.yelped = False
+  return len(yelpers)
+
+
+def med_visits(model):
+  bus_visits = [bus.number_of_visitors for bus in model.schedule.agents if isinstance(bus, c.Commercial)]
+  return np.median(bus_visits)
+
+
+def med_pop(model):
+  bus_pop = [bus.popularity for bus in model.schedule.agents if isinstance(bus, c.Commercial)]
+  return np.median(bus_pop)
+
+
+def vacant_high(model):
+  properties = [prop for prop in model.schedule.agents if
+                isinstance(prop, r.Residential) and prop.vacancy and prop.property_value == 2]
+  return len(properties)
---
+++ gentrification/main.py
@@ -0,0 +1,116 @@
+from mesa.visualization.ModularVisualization import ModularServer
+from mesa.visualization.UserParam import UserSettableParameter
+from mesa.visualization.modules import CanvasGrid
+from mesa.visualization.modules import ChartModule
+
+import commercial as c
+import person as p
+import residential as r
+import street as s
+import model as m
+
+def agent_portrayal(agent):
+  if agent is None:
+    return
+
+  portrayal = {"Shape": "circle",
+               "Filled": "true",
+               "r": 0.5}
+
+  if type(agent) is p.Person:
+    portrayal["Layer"] = 1
+    portrayal["r"] = 0.4
+    # if agent.yelped:
+    #   portrayal["Color"] = "black"
+    if agent.econ_status == 0:
+      portrayal["Color"] = "#A9F5A9"
+    elif agent.econ_status == 1:
+      portrayal["Color"] = "#01DF01"
+    elif agent.econ_status == 2:
+      portrayal["Color"] = "#088A08"
+    else:
+      portrayal["Shape"] = "rect"
+      portrayal["Color"] = "black"
+
+  elif type(agent) is r.Residential:
+    portrayal["Shape"] = "rect"
+    portrayal["Layer"] = 0
+    portrayal["w"] = 1
+    portrayal["h"] = 1
+    if agent.vacancy:
+      portrayal["Color"] = "white"
+    elif agent.property_value == 0:
+      portrayal["Color"] = "#E3CEF6"
+    elif agent.property_value == 1:
+      portrayal["Color"] = "#BE81F7"
+    elif agent.property_value == 2:
+      portrayal["Color"] = "#7401DF"
+    else:
+      portrayal["Shape"] = "circle"
+      portrayal["Color"] = "#7401DF"
+      portrayal["r"] = 0.7
+
+  elif type(agent) is c.Commercial:
+    portrayal["Shape"] = "rect"
+    portrayal["Layer"] = 0
+    portrayal["w"] = 1
+    portrayal["h"] = 1
+    if agent.econ_status == 0:
+      portrayal["Color"] = "#F6CECE"
+    elif agent.econ_status == 1:
+      portrayal["Color"] = "#F78181"
+    elif agent.econ_status == 2:
+      portrayal["Color"] = "#DF0101"
+    else:
+      portrayal["Shape"] = "circle"
+      portrayal["Color"] = "#DF0101"
+      portrayal["r"] = 0.7
+
+  elif type(agent) is s.Street:
+    portrayal["Color"] = "grey"
+    portrayal["Shape"] = "rect"
+    portrayal["Layer"] = 0
+    portrayal["w"] = 1
+    portrayal["h"] = 1
+
+  return portrayal
+
+
+if __name__ == '__main__':
+  grid = CanvasGrid(agent_portrayal, 20, 20, 500, 500)
+
+  chart = ChartModule([
+    {"Label": "Median Property Value", "Color": "Black"},
+    {"Label": "Median Economic Status of Residents", "Color": "Red"},
+    {"Label": "Median Economic Status of Visitors", "Color": "Blue"}
+  ], data_collector_name='datacollector')
+
+  chart2 = ChartModule([
+    {"Label": "Vacant homes with high property value", "Color": "Black"}
+  ], data_collector_name='datacollector')
+
+  chart3 = ChartModule([
+    {"Label": "Median popularity of businesses", "Color": "Black"}
+  ], data_collector_name='datacollector')
+
+  chart4 = ChartModule([
+    {"Label": "Number of people sharing", "Color": "Black"}
+  ], data_collector_name='datacollector')
+
+  chart5 = ChartModule([
+    {"Label": "Median number of visitors to businesses", "Color": "Black"}
+  ], data_collector_name='datacollector')
+
+  n_slider = UserSettableParameter('slider', "Number of Initial Residents", 250, 100, 360, 10)
+  p_slider = UserSettableParameter('slider', "Number of People Outside the Neighbourhood", 100, 10, 700, 10)
+  h_slider = UserSettableParameter('slider', "Number of Homes", 260, 100, 360, 10)
+  c_slider = UserSettableParameter('slider', "Number of Businesses", 70, 20, 360, 10)
+
+  server = ModularServer(
+      m.Gentrified_Neighbourhood,
+      [grid, chart, chart2, chart3, chart4, chart5],
+      "Gentrified Neighbourhood",
+      {"N": 120, "I": n_slider, "J": p_slider, "R": h_slider, "C": c_slider, "S": 10, "width": 20, "height": 20})
+
+  server.port = 8632
+  server.launch()
